4c7a8a9dda024b9910ead2b518fb39c029457b5f
Paresh Navalakha <paresh.navalakha@gmail.com>
2017-08-23 19:22:03 +0530

    Added Unit Tests

--- src/test/java/com/paresh/diff/util/test/DiffCalculationEngineTest.java
+++ src/test/java/com/paresh/diff/util/test/DiffCalculationEngineTest.java
@@ -1,17 +1,5 @@
 package com.paresh.diff.util.test;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.paresh.diff.dto.ChangeType;
 import com.paresh.diff.dto.Diff;
 import com.paresh.diff.dto.DiffResponse;
@@ -19,180 +7,189 @@ import com.paresh.diff.util.DiffComputeEngine;
 import com.paresh.diff.util.test.TestDataProvider.Address;
 import com.paresh.diff.util.test.TestDataProvider.AddressBuilder;
 import com.paresh.diff.util.test.TestDataProvider.Person;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
 
 public class DiffCalculationEngineTest {
-	private static final Logger LOGGER = LoggerFactory.getLogger(DiffCalculationEngineTest.class);
-	private DiffComputeEngine diffComputeEngine;
-	
-	@Before
-	public void setUp() {
-		this.diffComputeEngine = DiffComputeEngine.getInstance();
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(DiffCalculationEngineTest.class);
+    private DiffComputeEngine diffComputeEngine;
+
+    @Before
+    public void setUp() {
+        this.diffComputeEngine = DiffComputeEngine.getInstance();
+    }
 
     @Test
     public void testSameObjects() {
-    	Integer age = 26;
-    	String name = "Danniel";
-    	List<Address> addresses = new ArrayList<>();
-    	addresses.add(new AddressBuilder().setCity("LA").build());
-    	addresses.add(new AddressBuilder().setCity("NY").build());
-    	
-    	Map<String,String> attributes = new HashMap<>();
-        attributes.put("Height","180");
-        attributes.put("Weight","95");
+        Integer age = 26;
+        String name = "Danniel";
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(new AddressBuilder().setCity("LA").build());
+        addresses.add(new AddressBuilder().setCity("NY").build());
 
-    	Person dann = TestDataProvider.getPerson(age, name, addresses, attributes);
-    	Person sameDann = TestDataProvider.getPerson(age, name, addresses, attributes);
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("Height", "180");
+        attributes.put("Weight", "95");
 
-		DiffResponse differences = diffComputeEngine.findDifferences(dann, sameDann);
+        Person dann = com.paresh.diff.util.test.TestDataProvider.getPerson(age, name, addresses, attributes);
+        Person sameDann = com.paresh.diff.util.test.TestDataProvider.getPerson(age, name, addresses, attributes);
 
-		printResults(differences);
-		//Assert the response
-		Assert.assertTrue("Response should not be null", differences != null);
-		Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
+        DiffResponse differences = diffComputeEngine.findDifferences(dann, sameDann);
 
-		differences.getDiffs()
-			.forEach(diff -> {
-				Assert.assertEquals("Change type should be Updated", ChangeType.NO_CHANGE, diff.getChangeType());
-				Assert.assertEquals("Before and after should be same", diff.getBefore(), diff.getAfter());
-			});
+        printResults(differences);
+        //Assert the response
+        Assert.assertTrue("Response should not be null", differences != null);
+        Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
+
+        differences.getDiffs()
+                .forEach(diff -> {
+                    Assert.assertEquals("Change type should be Updated", ChangeType.NO_CHANGE, diff.getChangeType());
+                    Assert.assertEquals("Before and after should be same", diff.getBefore(), diff.getAfter());
+                });
     }
 
     @Test
     public void testChangedObjectWithSimpleAttribute() {
-    	Integer age = 26;
-    	String name = "Danniel";
-    	Integer changedAge = 28;
-    	List<Address> addresses = new ArrayList<>();
-    	addresses.add(new AddressBuilder().setCity("LA").build());
-    	addresses.add(new AddressBuilder().setCity("NY").build());
+        Integer age = 26;
+        String name = "Danniel";
+        Integer changedAge = 28;
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(new AddressBuilder().setCity("LA").build());
+        addresses.add(new AddressBuilder().setCity("NY").build());
 
-    	Map<String,String> attributes = new HashMap<>();
-        attributes.put("Height","180");
-        attributes.put("Weight","95");
-    	
-    	Person dann = TestDataProvider.getPerson(age, name, addresses, attributes);
-    	Person changedDann = TestDataProvider.getPerson(changedAge, name, addresses, attributes);
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("Height", "180");
+        attributes.put("Weight", "95");
 
-		DiffResponse differences = diffComputeEngine.findDifferences(dann, changedDann);
+        Person dann = com.paresh.diff.util.test.TestDataProvider.getPerson(age, name, addresses, attributes);
+        Person changedDann = com.paresh.diff.util.test.TestDataProvider.getPerson(changedAge, name, addresses, attributes);
 
-		printResults(differences);
-		//Assert the response
-		Assert.assertTrue("Response should not be null", differences != null);
-		Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
+        DiffResponse differences = diffComputeEngine.findDifferences(dann, changedDann);
 
-		differences.getDiffs().stream()
-			.forEach(diff -> {
-				Assert.assertEquals("Change type should be Updated", ChangeType.UPDATED, diff.getChangeType());
-				diff.getChildDiffs()
-					.stream()
-					.filter(childDiff -> childDiff.getFieldDescription().equals("Age"))
-					.forEach(childDiff -> {
-						Assert.assertEquals("Change type should be Updated", ChangeType.UPDATED, childDiff.getChangeType());
-						Assert.assertEquals("Before Age should be same", age.toString(), childDiff.getBefore());
-						Assert.assertEquals("After Age should be same", changedAge.toString(), childDiff.getAfter());
-					});
-			});;
+        printResults(differences);
+        //Assert the response
+        Assert.assertTrue("Response should not be null", differences != null);
+        Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
+
+        differences.getDiffs().stream()
+                .forEach(diff -> {
+                    Assert.assertEquals("Change type should be Updated", ChangeType.UPDATED, diff.getChangeType());
+                    diff.getChildDiffs()
+                            .stream()
+                            .filter(childDiff -> childDiff.getFieldDescription().equals("Age"))
+                            .forEach(childDiff -> {
+                                Assert.assertEquals("Change type should be Updated", ChangeType.UPDATED, childDiff.getChangeType());
+                                Assert.assertEquals("Before Age should be same", age.toString(), childDiff.getBefore());
+                                Assert.assertEquals("After Age should be same", changedAge.toString(), childDiff.getAfter());
+                            });
+                });
+        ;
     }
 
     @Test
     public void testFirstNullArgument() {
-    	Integer age = 26;
-    	String name = "Danniel";
-    	List<Address> addresses = new ArrayList<>();
-    	addresses.add(new AddressBuilder().setCity("LA").build());
-    	addresses.add(new AddressBuilder().setCity("NY").build());
+        Integer age = 26;
+        String name = "Danniel";
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(new AddressBuilder().setCity("LA").build());
+        addresses.add(new AddressBuilder().setCity("NY").build());
 
-    	Map<String,String> attributes = new HashMap<>();
-        attributes.put("Height","180");
-        attributes.put("Weight","95");
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("Height", "180");
+        attributes.put("Weight", "95");
 
-    	Person dann = TestDataProvider.getPerson(age, name, addresses, attributes);
+        Person dann = com.paresh.diff.util.test.TestDataProvider.getPerson(age, name, addresses, attributes);
 
-		DiffResponse differences = diffComputeEngine.findDifferences(null, dann);
+        DiffResponse differences = diffComputeEngine.findDifferences(null, dann);
 
-		printResults(differences);
-		//Assert the response
-		Assert.assertTrue("Response should not be null", differences != null);
-		Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
-		differences.getDiffs().forEach(diff -> {
-			Assert.assertEquals("Change type should be Deleted", ChangeType.ADDED, diff.getChangeType());
-			Assert.assertEquals("Field description should be null", null, diff.getFieldDescription());
-		});
+        printResults(differences);
+        //Assert the response
+        Assert.assertTrue("Response should not be null", differences != null);
+        Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
+        differences.getDiffs().forEach(diff -> {
+            Assert.assertEquals("Change type should be Deleted", ChangeType.ADDED, diff.getChangeType());
+            Assert.assertEquals("Field description should be null", null, diff.getFieldDescription());
+        });
     }
-    
+
     @Test
     public void testSecondNullArgument() {
-    	Integer age = 26;
-    	String name = "Danniel";
-    	List<Address> addresses = new ArrayList<>();
-    	addresses.add(new AddressBuilder().setCity("LA").build());
-    	addresses.add(new AddressBuilder().setCity("NY").build());
+        Integer age = 26;
+        String name = "Danniel";
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(new AddressBuilder().setCity("LA").build());
+        addresses.add(new AddressBuilder().setCity("NY").build());
 
-    	Map<String,String> attributes = new HashMap<>();
-        attributes.put("Height","180");
-        attributes.put("Weight","95");
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("Height", "180");
+        attributes.put("Weight", "95");
 
-    	Person dann = TestDataProvider.getPerson(age, name, addresses, attributes);
+        Person dann = com.paresh.diff.util.test.TestDataProvider.getPerson(age, name, addresses, attributes);
 
-		DiffResponse differences = diffComputeEngine.findDifferences(dann, null);
+        DiffResponse differences = diffComputeEngine.findDifferences(dann, null);
 
-		printResults(differences);
-		//Assert the response
-		Assert.assertTrue("Response should not be null", differences != null);
-		Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
-		differences.getDiffs().forEach(diff -> {
-			Assert.assertEquals("Change type should be Deleted", ChangeType.DELETED, diff.getChangeType());
-			Assert.assertEquals("Field description should be null", null, diff.getFieldDescription());
-		});
+        printResults(differences);
+        //Assert the response
+        Assert.assertTrue("Response should not be null", differences != null);
+        Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
+        differences.getDiffs().forEach(diff -> {
+            Assert.assertEquals("Change type should be Deleted", ChangeType.DELETED, diff.getChangeType());
+            Assert.assertEquals("Field description should be null", null, diff.getFieldDescription());
+        });
     }
 
     @Test
     public void testChangedObjectWithNullComplexAttribute() {
-    	Integer age = 26;
-    	String name = "Danniel";
-    	Integer changedAge = 28;
-    	List<Address> addresses = new ArrayList<>();
-    	addresses.add(new AddressBuilder().setCity("LA").build());
-    	addresses.add(new AddressBuilder().setCity("NY").build());
+        Integer age = 26;
+        String name = "Danniel";
+        Integer changedAge = 28;
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(new AddressBuilder().setCity("LA").build());
+        addresses.add(new AddressBuilder().setCity("NY").build());
 
-    	Person dann = TestDataProvider.getPerson(age, name, addresses, null);
-    	Person changedDann = TestDataProvider.getPerson(changedAge, name, addresses, null);
+        Person dann = com.paresh.diff.util.test.TestDataProvider.getPerson(age, name, addresses, null);
+        Person changedDann = com.paresh.diff.util.test.TestDataProvider.getPerson(changedAge, name, addresses, null);
 
-		DiffResponse differences = diffComputeEngine.findDifferences(dann, changedDann);
+        DiffResponse differences = diffComputeEngine.findDifferences(dann, changedDann);
 
-		printResults(differences);
-		//Assert the response
-		Assert.assertTrue("Response should not be null", differences != null);
-		Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
+        printResults(differences);
+        //Assert the response
+        Assert.assertTrue("Response should not be null", differences != null);
+        Assert.assertTrue("Response should contain Collection of diff objects", differences.getDiffs().size() > 0);
 
-		differences.getDiffs().stream()
-			.forEach(diff -> {
-				Assert.assertEquals("Change type should be Updated", ChangeType.UPDATED, diff.getChangeType());
-				diff.getChildDiffs()
-					.forEach(childDiff -> {
-						if(childDiff.getFieldDescription().equals("Age")) {
-							Assert.assertEquals("Change type should be Updated", ChangeType.UPDATED, childDiff.getChangeType());
-							Assert.assertEquals("Before Age should be same", age.toString(), childDiff.getBefore());
-							Assert.assertEquals("After Age should be same", changedAge.toString(), childDiff.getAfter());
-						} else if(childDiff.getFieldDescription().equals("Name")) {
-							Assert.assertEquals("Change type should be Updated", ChangeType.NO_CHANGE, childDiff.getChangeType());
-						} else if(childDiff.getFieldDescription().equals("Attributes")) {
-							Assert.assertEquals("Change type should be Updated", ChangeType.NO_CHANGE, childDiff.getChangeType());
-						}
-					});
-			});;
+        differences.getDiffs().stream()
+                .forEach(diff -> {
+                    Assert.assertEquals("Change type should be Updated", ChangeType.UPDATED, diff.getChangeType());
+                    diff.getChildDiffs()
+                            .forEach(childDiff -> {
+                                if (childDiff.getFieldDescription().equals("Age")) {
+                                    Assert.assertEquals("Change type should be Updated", ChangeType.UPDATED, childDiff.getChangeType());
+                                    Assert.assertEquals("Before Age should be same", age.toString(), childDiff.getBefore());
+                                    Assert.assertEquals("After Age should be same", changedAge.toString(), childDiff.getAfter());
+                                } else if (childDiff.getFieldDescription().equals("Name")) {
+                                    Assert.assertEquals("Change type should be Updated", ChangeType.NO_CHANGE, childDiff.getChangeType());
+                                } else if (childDiff.getFieldDescription().equals("Attributes")) {
+                                    Assert.assertEquals("Change type should be Updated", ChangeType.NO_CHANGE, childDiff.getChangeType());
+                                }
+                            });
+                });
+        ;
     }
-    
+
     @Test
     public void testChangedObjects() {
-        List<Person> afterTestData = TestDataProvider.getAfterPersonList();
-		List<Person> beforeTestData = TestDataProvider.getBeforePersonList();
-		DiffComputeEngine diffComputeEngine = DiffComputeEngine.getInstance();
+        List<Person> afterTestData = com.paresh.diff.util.test.TestDataProvider.getAfterPersonList();
+        List<Person> beforeTestData = com.paresh.diff.util.test.TestDataProvider.getBeforePersonList();
+        DiffComputeEngine diffComputeEngine = DiffComputeEngine.getInstance();
 
-		DiffResponse differences = diffComputeEngine.findDifferences(beforeTestData, afterTestData);
+        DiffResponse differences = diffComputeEngine.findDifferences(beforeTestData, afterTestData);
 
-		printResults(differences);
+        printResults(differences);
     }
 
     private void printResults(DiffResponse diffResponse) {
---
+++ src/test/java/com/paresh/diff/util/test/TestDataProvider.java
@@ -0,0 +1,211 @@
+package com.paresh.diff.util.test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.paresh.diff.annotations.Identifier;
+
+public class TestDataProvider {
+
+	public static Person getPerson(final int age, final String name, final List<Address> addresses,
+			final Map<String, String> attributes) {
+		return new PersonBuilder().setAge(age).setName(name).setAddresses(addresses).setAttributes(attributes).build();
+	}
+
+    public static List<Person> getBeforePersonList() {
+        List<Address> addressList1 = Arrays.asList(new AddressBuilder().setCity("Delhi").build(),
+                new AddressBuilder().setCity("Hong Kong").build());
+
+        Map<String,String> attributes1 = new HashMap<>();
+        attributes1.put("Height","175");
+        attributes1.put("Weight","72");
+        attributes1.put("Hair Color","Black");
+
+        Map<String,String> attributes2 = new HashMap<>();
+        attributes2.put("Height","180");
+        attributes2.put("Weight","95");
+
+        List<Address> addressList2 = Arrays.asList(new AddressBuilder().setCity("Delhi").build(),
+                new AddressBuilder().setCity("Mumbai").build());
+        return Arrays.asList(
+                new PersonBuilder().setAge(36).setName("Sam Adams").setAddresses(addressList1).setAttributes(attributes1).build(),
+                new PersonBuilder().setAge(32).setName("Jolly Adams").setAddresses(addressList2).setAttributes(attributes2).build());
+
+    }
+
+    public static List<Person> getAfterPersonList() {
+        Map<String,String> attributes1 = new HashMap<>();
+        attributes1.put("Height","175");
+        attributes1.put("Weight","78");
+
+        Map<String,String> attributes2 = new HashMap<>();
+        attributes2.put("Height","180");
+        attributes2.put("Weight","100");
+        attributes2.put("Hair Color","Red");
+        List<Address> addressList3 = Arrays.asList(new AddressBuilder().setCity("Delhi").build(),
+                new AddressBuilder().setCity("New York").build());
+
+        return Arrays.asList(new PersonBuilder().setAge(37).setName("Sam Adams").setAttributes(attributes1).build(),
+                new PersonBuilder().setAge(33).setName("Jolly Adams").setAttributes(attributes2).setAddresses(addressList3).build());
+    }
+
+    private static class PersonBuilder {
+        private String name;
+        private int age;
+        private List<Address> addresses;
+        private Map<String,String> attributes;
+
+        public PersonBuilder setAttributes(Map<String, String> attributes) {
+            this.attributes = attributes;
+            return this;
+        }
+
+        public PersonBuilder setAddresses(List<Address> addresses) {
+            this.addresses = addresses;
+            return this;
+        }
+
+        public PersonBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PersonBuilder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Person build() {
+            Person person = new Person();
+            person.setAge(age);
+            person.setName(name);
+            person.setAddresses(addresses);
+            person.setAttributes(attributes);
+            return person;
+        }
+
+    }
+
+    public static class AddressBuilder {
+        private String city;
+
+        public AddressBuilder setCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Address build() {
+            Address address = new Address();
+            address.setCity(city);
+            return address;
+        }
+
+    }
+
+    public static class Address {
+        private String city;
+
+        @Identifier
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        @Override
+        public String toString() {
+            return "Address{" + "city=" + city + '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            Address address = (Address) o;
+
+            return city != null ? city.equals(address.city) : address.city == null;
+        }
+
+        @Override
+        public int hashCode() {
+            return city != null ? city.hashCode() : 0;
+        }
+    }
+
+    public static class Person {
+        private String name;
+        private int age;
+        private List<Address> addresses;
+        private Map<String, String> attributes;
+
+        public Map<String, String> getAttributes() {
+            return attributes;
+        }
+
+        public void setAttributes(Map<String, String> attributes) {
+            this.attributes = attributes;
+        }
+
+        public List<Address> getAddresses() {
+            return addresses;
+        }
+
+        public void setAddresses(List<Address> addresses) {
+            this.addresses = addresses;
+        }
+
+        @Identifier
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            Person person = (Person) o;
+
+            if (age != person.age)
+                return false;
+            if (name != null ? !name.equals(person.name) : person.name != null)
+                return false;
+            return addresses != null ? addresses.equals(person.addresses) : person.addresses == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + age;
+            result = 31 * result + (addresses != null ? addresses.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" + "name=" + name + '}';
+        }
+
+    }
+}
