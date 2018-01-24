4ff70c561d6c21eae04f9ee470e155e3d23c04f8
Paresh Navalakha <paresh.navalakha@gmail.com>
2018-01-25 00:31:05 +0530

    Minor change

--- src/main/java/com/paresh/diff/dto/ChangeType.java
+++ src/main/java/com/paresh/diff/dto/ChangeType.java
@@ -4,5 +4,16 @@ package com.paresh.diff.dto;
  * Created by Admin on 27-06-2017.
  */
 public enum ChangeType {
-    UPDATED, NO_CHANGE, ADDED, DELETED
+    UPDATED("Updated"), NO_CHANGE("Unchanged"), ADDED("New"), DELETED("Deleted");
+
+    private String description;
+
+    ChangeType(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
 }
